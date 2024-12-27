2c2a167ae36a7f02668140759eb3934e22fdc601
Rodriguez Ossa Alexis Kevin <alexisrodriguezossa99@gmail.com>
2024-12-26 22:09:33 -0300

    Front ready

--- database/config.js
+++ database/config.js
@@ -4,10 +4,10 @@ const mongoose = require('mongoose');
 const dbConnection = async() => {
 
     try {
-        console.log("process.env.DB_CNN °°°°");
+        // console.log("process.env.DB_CNN °°°°");
 
-        console.log(process.env.DB_CNN);
-        console.log(process.env.SECRET_JWT_SEED);
+        // console.log(process.env.DB_CNN);
+        // console.log(process.env.SECRET_JWT_SEED);
 
         await mongoose.connect( process.env.DB_CNN 
             , {
--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 require('dotenv').config();
 const cors = require('cors');
 const { dbConnection } = require('./database/config');
-
+const path = require('path');
 // Crear el servidor de express
 const app = express();
 
@@ -22,7 +22,9 @@ app.use( express.json() );
 app.use('/api/auth', require('./routes/auth') );
 app.use('/api/events', require('./routes/events') );
 
-
+app.use('*',(req,res) => {
+    res.sendFile(path.join(__dirname,'public/index.html'))
+})
 
 
 // Escuchar peticiones
